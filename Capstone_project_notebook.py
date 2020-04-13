aff730c8033aa7caf475dcaa1f9cbea339db27bb
Flucomic <e.f.mouresan@hotmail.com>
2020-04-13 20:07:51 +0200

    adds my working capstone notebook

---
+++ Capstone_project_notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for the Capstone project of the IBM Data Science certificate on Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
